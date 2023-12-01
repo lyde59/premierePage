5a1dfbd05ad0f1b3524f6a4cbffd071df7dcdbcc
lyde59 <lyde-59@hotmail.fr>
2023-12-01 12:00:42 +0100

    tictacdo completer

--- script/games.js
+++ script/games.js
@@ -79,6 +79,9 @@ function TicTacDoe(){
     /* déclaation des variables */
 
     let squares=[]
+    let isActive=[]
+    let score=[]
+    let redPlayer = true;
     
     /*mise en page de la zone */
     let infoPanel = document.createElement('div')
@@ -99,21 +102,79 @@ for(let i=0; i<9; i++){
     square.classList.add('square')
     grid.appendChild(square)
     squares.push(square)
+    isActive.push(true)
+    score.push(0)
 }
 
 for(let i=0; i<9; i++){
-    squares[i].addEventListener('click',squareClick.bind(squares[i]))
+    squares[i].addEventListener('click',squareClick.bind(squares[i],i))
 }
 
-console.log(squares)
+console.log(squares, isActive, score)
 
-function squareClick(){
+/*fonctionnement du jeu*/
+
+/**
+ * déclenchée quand on clique sur un carré de la grille
+ */
+function squareClick(squareNumber){
+    if(isActive[squareNumber]) {
+        if(redPlayer){
     this.style.backgroundImage = 'url("../assets/rouge.png")'
+    score[squareNumber] = 1
+    } else{
+        this.style.backgroundImage='url("../assets/vert.png")'
+        score[squareNumber] = 4
+        }
+        isActive[squareNumber]=false
+        redPlayer = !redPlayer
+        checkVictory()
+    }
     }
 
-}
+    function checkVictory(){
+        let lineScore = [
+            score[0]+score[1]+score[2],
+            score[3]+score[4]+score[5],
+            score[6]+score[7]+score[8],
+            score[0]+score[3]+score[6],
+            score[1]+score[4]+score[7],
+            score[2]+score[5]+score[8],
+            score[0]+score[4]+score[8],
+            score[2]+score[4]+score[6]
+        ]
+        let endGameMessage=""
+        if(lineScore.includes(3)){
+            endGameMessage="Victoire Rouge"
+            endGame(endGameMessage)
+        }else if (lineScore.includes(12)){
+            endGameMessage="Victoire Vert"
+            endGame(endGameMessage)
+        }else if (!isActive.includes(true)) {
+            endGameMessage="Egalité"
+            endGame(endGameMessage)
+        }
+        console.log(lineScore, endGameMessage)
+    }
 
+function endGame(endGameMessage){
+    for(let i=0; i<8; i++){
+        isActive[i]=false
+
+    }
+    let gameOverMessage=document.createElement('h2')
+    gameOverMessage.classList.add('gameOver')
+    gameOverMessage.innerHTML=endGameMessage
+
+    infoPanel.appendChild(gameOverMessage)
+
+    let restartBut = document.createElement('button')
+    restartBut.innerHTML="Recommencé"
+    restartBut.addEventListener('click',TicTacDoe)
+
+    infoPanel.appendChild(restartBut)
+}
+}
 /* on configure les évènements*/
 
 menu.addEventListener("change", menuChange)
- 
